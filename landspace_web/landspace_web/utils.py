af0240a7a456bf10cfe25b1bf1d1961fc1e4d1b4
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-01-29 13:09:15 -0700

    make sure to check if user doesnt exist before rejecting fb auth for new user not allowed

--- landspace_web/landspace_web/utils.py
+++ landspace_web/landspace_web/utils.py
@@ -1,5 +1,5 @@
 from social_core.exceptions import AuthForbidden
 
-def auth_allowed(backend, strategy, response, *args, **kwargs):
-	if strategy.session_get('social_auth_new_user_allowed', '0') == '0':
+def auth_allowed(backend, strategy, user=None, *args, **kwargs):
+	if strategy.session_get('social_auth_new_user_allowed', '0')=='0' and user==None:
 		return strategy.redirect('/users/login/badfbauth/')
