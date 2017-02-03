796fc1519648693ed7e14326122647437604709d
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-02-03 06:53:42 -0700

    moving drf and future assets to webpack dev server for dev only

--- landspace_web/landspace_web/settings/dev.py
+++ landspace_web/landspace_web/settings/dev.py
@@ -7,4 +7,7 @@ EMAIL_PORT = 1025
 SOCIAL_AUTH_FACEBOOK_KEY='1862275167388345'
 SOCIAL_AUTH_FACEBOOK_SECRET='e8fd4bd0685e9cab6f2928177fd53f82'
 
+# Settings for Webpack Dev Server
 STATIC_URL = 'http://localhost:9000/'
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "static")
+STATICFILES_DIRS = ()
