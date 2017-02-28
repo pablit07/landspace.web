f5a548a117f18886e1529587d8d9b7d38c555516
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2017-02-27 17:17:45 -0700

    designer projects folder icon and tabs

--- src/components/DesignerProjectsPage.js
+++ src/components/DesignerProjectsPage.js
@@ -40,9 +40,37 @@ export default class DesignerProjectsPage extends React.Component {
     	var projectComponents = [];
 
     	this.state.designerProjects.forEach((project) => {
-    		projectComponents.push(<div className='project-container'>{project.name}</div>);
+    		projectComponents.push(<div className='project-container'>
+			    						<div className='overlay'></div>
+			    						<i className="fa fa-folder-o fa-10x background" aria-hidden="true"></i>
+				    					<div className='title'>{project.name}</div>
+								</div>);
     	});
 
-		return (<div className='ink-grid'>{projectComponents}</div>);
+		return (<div className='ink-grid full-height'>
+					<div className="ink-tabs top full-height left-space right-space vertical-space" data-prevent-url-change="true">
+					    <ul className="tabs-nav">
+					        <li><a className="tabs-tab" href="#home">Active Projects</a></li>
+					        <li><a className="tabs-tab" href="#history">History</a></li>
+					    </ul>
+
+					    <div id="home" className="tabs-content full-height">
+							<div className='column-group vertical-space'>
+		    					<div className='all-90'>
+		    					{projectComponents}
+		    					</div>
+							</div>
+						</div>
+
+						<div id="history" className="tabs-content full-height">
+
+							<div className="column-group push-center push-middle gutters vertical-space">
+							    <div className="all-50 push-center align-center">
+									<i className="fa fa-refresh fa-spin fa-4x fa-fw"></i>
+							    </div>
+							</div>
+						</div>
+					</div>
+			</div>);
 	}
 }
